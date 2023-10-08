291cd7e7959a9e332436ba78aa74b19642ba6027
DilaJumrotinF <dillafadila11@gmail.com>
2023-10-08 10:46:57 +0700

    foreignKey update

--- models/associations.js
+++ models/associations.js
@@ -1,6 +1,6 @@
 const Konseling = require('./konseling.model');
 const Testimoni = require('./testimoni.model');
 
-Testimoni.belongsTo(Konseling, { foreignKey: 'nama_lengkap' });
+Testimoni.belongsTo(Konseling, { foreignKey: 'Gmail' });
 
 module.exports = { Testimoni, User };
