dbe504729e7642b6dc6c324d1846f02585624efd
DilaJumrotinF <dillafadila11@gmail.com>
2023-10-10 15:10:38 +0700

    port update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/db.config');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 const testimoniRoute = require('./routes/testimoni.routes');
 const konselingRoute = require('./routes/konseling.routes');
 
